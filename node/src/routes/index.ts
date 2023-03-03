8e55d6c66b9da78b2630a9f45ae380d224837981
Adrian Trujillo <adrian.td96@gmail.com>
2023-03-03 12:07:01 -0600

    feat: :sparkles: added health route
    Added route that checks if the server is listening.

---
+++ node/src/routes/index.ts
@@ -0,0 +1,3 @@
+import healthRouter from './health';
+
+export { healthRouter };
--- node/src/server.ts
+++ node/src/server.ts
@@ -1,12 +1,15 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import morgan from 'morgan';
+import { healthRouter } from './routes';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+	morgan(':method :url :status :res[content-length] - :response-time ms')
+);
 
-app.get('/', (_req: Request, res: Response) => {
-	res.send('Hello World!');
-});
+app.use('/api/_health', healthRouter);
 
 export default app;
