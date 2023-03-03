aa11117868684f6ff076e759a3d524fd82714d21
Adrian Trujillo <adrian.td96@gmail.com>
2023-03-03 14:07:02 -0600

    test: :white_check_mark: added userService test
    Added test to check functionality of the userService function.

---
+++ node/src/tests/services/userService.test.ts
@@ -0,0 +1,25 @@
+import { describe, it } from 'mocha';
+import { expect } from 'chai';
+
+import userService from '../../services/userService';
+
+describe('userService', function () {
+	it('should return a user', async function () {
+		const expectedUser = {
+			username: 'test',
+			password: 'hashedPassword',
+			salt: 'salt',
+			role: 'testRole',
+		};
+		const user = await userService.findUser('test');
+		expect(user).to.deep.equal(expectedUser);
+	});
+
+	it('should throw an error if user is not found', async function () {
+		try {
+			await userService.findUser('notAUser');
+		} catch (err) {
+			expect(err.message).to.equal('User not found');
+		}
+	});
+});
