a86c87e87658613883a052a79e392278ecf7831a
Adrian Trujillo <adrian.td96@gmail.com>
2023-03-05 23:26:37 -0600

    fix: :white_check_mark: updated userService test
    Updated test because the db source changed.

--- node/src/tests/services/userService.test.ts
+++ node/src/tests/services/userService.test.ts
@@ -6,13 +6,17 @@ import userService from '../../services/userService';
 describe('userService', function () {
 	it('should return a user', async function () {
 		const expectedUser = {
-			username: 'test',
-			password: 'hashedPassword',
-			salt: 'salt',
-			role: 'testRole',
+			username: 'bob',
+			role: 'viewer',
 		};
-		const user = await userService.findUser('test');
-		expect(user).to.deep.equal(expectedUser);
+		const user = await userService.findUser(expectedUser.username);
+		expect(user).to.be.an('object');
+		expect(user).to.have.property('username');
+		expect(user).to.have.property('password');
+		expect(user).to.have.property('salt');
+		expect(user).to.have.property('role');
+		expect(user.username).to.equal(expectedUser.username);
+		expect(user.role).to.equal(expectedUser.role);
 	});
 
 	it('should throw an error if user is not found', async function () {
