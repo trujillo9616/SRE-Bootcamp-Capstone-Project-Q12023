97d4304fb247fa9d515fba952a00324c352e9369
Adrian Trujillo <adrian.td96@gmail.com>
2023-03-06 00:09:51 -0600

    refactor: :recycle: refactor index route
    Refactored index route for a cleaner server file.

--- node/src/routes/index.ts
+++ node/src/routes/index.ts
@@ -1,6 +1,17 @@
+import express, { Request, Response } from 'express';
 import healthRouter from './health';
 import loginRouter from './login';
 import cidrToMask from './cidrToMask';
 import maskToCidr from './maskToCidr';
 
-export { healthRouter, loginRouter, cidrToMask, maskToCidr };
+const apiRouter = express.Router();
+
+apiRouter.get('/', (_req: Request, res: Response) => {
+	res.send({ status: 'OK' });
+});
+apiRouter.use('/_health', healthRouter);
+apiRouter.use('/login', loginRouter);
+apiRouter.use('/cidrToMask', cidrToMask);
+apiRouter.use('/maskToCidr', maskToCidr);
+
+export default apiRouter;
