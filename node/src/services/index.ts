8575d830d03ef6f29a6ee0176653e726838392b2
Adrian Trujillo <adrian.td96@gmail.com>
2023-03-05 23:37:00 -0600

    fix: :zap: updated services exports

--- node/src/services/index.ts
+++ node/src/services/index.ts
@@ -1,3 +1,15 @@
 import loginService from './loginService';
+import calculationService from './calculationService';
+import authService from './authService';
+import tokenService from './tokenService';
+import userService from './userService';
+import passwordService from './passwordService';
 
-export { loginService };
+export {
+	loginService,
+	calculationService,
+	authService,
+	tokenService,
+	userService,
+	passwordService,
+};
