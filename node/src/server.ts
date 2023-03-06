f300624d77fb42aac19d6230338cb8be72073226
Adrian Trujillo <adrian.td96@gmail.com>
2023-03-06 00:11:49 -0600

    feat: :zap: update router and middleware
    Added the updated router and new security middleware.

--- node/src/server.ts
+++ node/src/server.ts
@@ -1,21 +1,24 @@
 import express from 'express';
 import morgan from 'morgan';
+import hpp from 'hpp';
+import helmet from 'helmet';
 
-import { healthRouter, loginRouter, cidrToMask, maskToCidr } from './routes';
+import apiRouter from './routes';
 import middleware from './utils/middleware';
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true, limit: '1kb' }));
+app.use(express.json({ limit: '1kb' }));
+
+app.use(hpp());
+app.use(helmet());
 app.use(
 	morgan(':method :url :status :res[content-length] - :response-time ms')
 );
 
-app.use('/api/_health', healthRouter);
-app.use('/api/login', loginRouter);
-app.use('/api/cidrToMask', cidrToMask);
-app.use('/api/maskToCidr', maskToCidr);
+app.use('/api', apiRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
