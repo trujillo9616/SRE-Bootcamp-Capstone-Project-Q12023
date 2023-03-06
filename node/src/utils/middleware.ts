08e4bbaa4ecf931198b6ecc389b989ae35dea7fc
Adrian Trujillo <adrian.td96@gmail.com>
2023-03-05 23:24:24 -0600

    refactor: :recycle: refactored errorhandler
    Refactored middleware to use switch.

--- node/src/utils/middleware.ts
+++ node/src/utils/middleware.ts
@@ -10,18 +10,33 @@ const errorHandler = (
 	res: Response,
 	next: NextFunction
 ) => {
-	if (error.name === 'CastError') {
-		return res.status(400).send({ error: 'malformatted id' });
-	} else if (error.name === 'ValidationError') {
-		return res.status(400).json({ error: error.message });
-	} else if (error.name === 'JsonWebTokenError') {
-		return res.status(401).json({ error: 'invalid token' });
-	} else if (error.name === 'TokenExpiredError') {
-		return res.status(401).json({ error: 'token expired' });
-	} else if (error.name === 'UserNotFound') {
-		return res.status(401).json({ error: error.message });
-	} else if (error.name === 'InvalidPasswordError') {
-		return res.status(401).json({ error: error.message });
+	switch (error.name) {
+		case 'CastError':
+			return res.status(400).send({ error: 'malformatted id' });
+		case 'ValidationError':
+			return res.status(400).json({ error: error.message });
+		case 'JsonWebTokenError':
+			return res.status(401).json({ error: 'invalid token' });
+		case 'TokenExpiredError':
+			return res.status(401).json({ error: 'token expired' });
+		case 'UserNotFound':
+			return res.status(401).json({ error: error.message });
+		case 'InvalidPasswordError':
+			return res.status(401).json({ error: error.message });
+		case 'ValueRequiredError':
+			return res.status(400).json({ error: error.message });
+		case 'ValueTypeError':
+			return res.status(400).json({ error: error.message });
+		case 'InvalidCidrError':
+			return res.status(400).json({ error: error.message });
+		case 'InvalidMaskError':
+			return res.status(400).json({ error: error.message });
+		case 'MissingTokenError':
+			return res.status(401).json({ error: error.message });
+		case 'InvalidTokenError':
+			return res.status(401).json({ error: error.message });
+		case 'TokenError':
+			return res.status(401).json({ error: error.message });
 	}
 	return next(error);
 };
