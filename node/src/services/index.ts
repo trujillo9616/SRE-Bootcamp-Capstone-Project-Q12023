a78a709a44e0e7263ad0b15cc2ce2b7e3481fede
Adrian Trujillo <adrian.td96@gmail.com>
2023-03-05 19:30:43 -0600

    feat: :sparkles: added loginService
    Added loginService which uses other services.

---
+++ node/src/services/index.ts
@@ -0,0 +1,3 @@
+import loginService from './loginService';
+
+export { loginService };
---
+++ node/src/services/loginService.ts
@@ -0,0 +1,15 @@
+import userService from './userService';
+import passwordService from './passwordService';
+import tokenService from './tokenService';
+
+export default {
+	generateToken: async (
+		username: string,
+		password: string,
+		expiration?: string
+	) => {
+		const user = await userService.findUser(username);
+		passwordService.verifyPassword(password, user.salt, user.password);
+		return tokenService.signToken({ role: user.role }, expiration || '1h');
+	},
+};
