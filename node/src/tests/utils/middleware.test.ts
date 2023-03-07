d7b5e81d94e6c1b4566e7add9206c45aaa6d2bfc
Adrian Trujillo <adrian.td96@gmail.com>
2023-03-07 17:13:34 -0600

    test: :white_check_mark: updated middleware tests
    Added tests for the errorHandler middleware.

--- node/src/tests/utils/middleware.test.ts
+++ node/src/tests/utils/middleware.test.ts
@@ -1,10 +1,43 @@
-import { describe, it } from 'mocha';
+import { describe, it, before, after } from 'mocha';
 import { expect } from 'chai';
 import supertest from 'supertest';
+import sinon from 'sinon';
+
+import { customError } from '../../utils/errors';
+import { authService, calculationService } from '../../services';
 
 import app from '../../server';
 
+let validateToken: sinon.SinonStub;
+let cidrToMask: sinon.SinonStub;
+let maskToCidr: sinon.SinonStub;
+
 describe('middleware', function () {
+	before(function () {
+		validateToken = sinon
+			.stub(authService, 'validateToken')
+			.onCall(0)
+			.returns({ role: 'test' })
+			.onCall(1)
+			.returns({ role: 'test' })
+			.onCall(2)
+			.throws(customError('Token missing', 'MissingTokenError'))
+			.onCall(3)
+			.throws(customError('Token invalid', 'InvalidTokenError'));
+		cidrToMask = sinon
+			.stub(calculationService, 'cidrToMask')
+			.throws(customError('Invalid CIDR!', 'InvalidCidrError'));
+		maskToCidr = sinon
+			.stub(calculationService, 'maskToCidr')
+			.throws(customError('Invalid mask!', 'InvalidMaskError'));
+	});
+
+	after(function () {
+		validateToken.restore();
+		cidrToMask.restore();
+		maskToCidr.restore();
+	});
+
 	describe('unknownEndpoint', function () {
 		it('should return 404 status and error message', async function () {
 			const response = await supertest(app)
@@ -13,6 +46,54 @@ describe('middleware', function () {
 				.expect('Content-Type', /application\/json/);
 			expect(response.body).to.have.property('error');
 			expect(response.body.error).to.equal('unknown endpoint');
+		});
+	});
+
+	describe('errorHandler', function () {
+		describe('InvalidCidrError', function () {
+			it('should return 400 status and error message', async function () {
+				const response = await supertest(app)
+					.get('/api/cidrToMask')
+					.query({ value: 'abc' })
+					.expect(400)
+					.expect('Content-Type', /application\/json/);
+				expect(response.body).to.have.property('error');
+				expect(response.body.error).to.equal('Invalid CIDR!');
+			});
+		});
+
+		describe('InvalidMaskError', function () {
+			it('should return 400 status and error message', async function () {
+				const response = await supertest(app)
+					.get('/api/maskToCidr')
+					.query({ value: 'abc' })
+					.expect(400)
+					.expect('Content-Type', /application\/json/);
+				expect(response.body).to.have.property('error');
+				expect(response.body.error).to.equal('Invalid mask!');
+			});
+		});
+
+		describe('MissingTokenError', function () {
+			it('should return 401 status and error message', async function () {
+				const response = await supertest(app)
+					.get('/api/maskToCidr')
+					.expect(401)
+					.expect('Content-Type', /application\/json/);
+				expect(response.body).to.have.property('error');
+				expect(response.body.error).to.equal('Token missing');
+			});
+		});
+
+		describe('InvalidTokenError', function () {
+			it('should return 401 status and error message', async function () {
+				const response = await supertest(app)
+					.get('/api/maskToCidr')
+					.expect(401)
+					.expect('Content-Type', /application\/json/);
+				expect(response.body).to.have.property('error');
+				expect(response.body.error).to.equal('Token invalid');
+			});
 		});
 	});
 });
