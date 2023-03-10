66a709b3bfd42b54765d6edfefbc8ae8545d2e12
Adrian Trujillo <adrian.td96@gmail.com>
2023-03-10 15:20:07 -0600

    refactor: :recycle: refactor code to serverless
    Refactored code from monolith server to a serverless approach.

--- node/src/index.ts
+++
@@ -1,8 +0,0 @@
-import Config, { ConfigType } from './config';
-import app from './server';
-
-const config: ConfigType = Config;
-
-app.listen(config.port, function () {
-	console.log(`🚀 Server is listening on port ${config.port}`);
-});
--- node/src/utils/exclude.ts
+++
@@ -1,8 +0,0 @@
-function exclude<T, K extends keyof T>(model: T, keys: K[]): Omit<T, K> {
-	for (const key of keys) {
-		delete model[key];
-	}
-	return model;
-}
-
-export default exclude;
