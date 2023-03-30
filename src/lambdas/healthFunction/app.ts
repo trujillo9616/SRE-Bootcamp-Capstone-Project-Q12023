b585669520a38be7d055a3ec917824a13fbec3e3
Adrian Trujillo <adrian.td96@gmail.com>
2023-03-30 01:35:17 -0500

    fix: :bug: return function to original

--- src/lambdas/healthFunction/app.ts
+++ src/lambdas/healthFunction/app.ts
@@ -5,7 +5,7 @@ export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGat
     return {
       statusCode: 200,
       body: JSON.stringify({
-        status: 'Hello Lambda World!',
+        status: 'OK',
       }),
     };
   } catch (error) {
--- src/lambdas/healthFunction/tests/unit/test-handler.test.ts
+++ src/lambdas/healthFunction/tests/unit/test-handler.test.ts
@@ -10,7 +10,7 @@ describe('Unit test for app handler', function () {
     expect(result.statusCode).toEqual(200);
     expect(result.body).toEqual(
       JSON.stringify({
-        status: 'Hello Lambda World!',
+        status: 'OK',
       }),
     );
   });
