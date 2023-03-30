4d4a9eca094fed136bdabb8b9790cb88abb92902
Adrian Trujillo <adrian.td96@gmail.com>
2023-03-30 11:43:53 -0500

    feat: :zap: update auth Function
    Changed to CUSTOM type for authorizer function.

--- src/lambdas/authFunction/app.ts
+++ src/lambdas/authFunction/app.ts
@@ -1,17 +1,23 @@
-import { APIGatewayTokenAuthorizerEvent, AuthResponse } from 'aws-lambda';
+import { APIGatewayRequestAuthorizerEvent, AuthResponse } from 'aws-lambda';
 import { generatePolicy, verifyToken } from './utils';
 import { SecretsManager } from './utils/secretsManager';
 
-export const lambdaHandler = async (event: APIGatewayTokenAuthorizerEvent): Promise<AuthResponse> => {
-  console.log('event', event);
+export const lambdaHandler = async (event: APIGatewayRequestAuthorizerEvent): Promise<AuthResponse> => {
   let authResponse: AuthResponse;
   const JWT_SECRET = await SecretsManager.getSecretValue('JWT_SECRET', 'us-east-2');
   try {
-    if (!event.authorizationToken) {
+    if (!event.headers) {
       throw new Error('Unauthorized');
     }
-    const token = event.authorizationToken.substring(7);
-    await verifyToken(token, JWT_SECRET as string);
+    const token = event.headers.Authorization || event.headers.authorization;
+    if (!token) {
+      throw new Error('Unauthorized');
+    }
+    const tokenParts = token.split(' ');
+    if (tokenParts.length !== 2 || tokenParts[0] !== 'Bearer') {
+      throw new Error('Unauthorized');
+    }
+    await verifyToken(tokenParts[1], JWT_SECRET as string);
     authResponse = {
       principalId: '*',
       policyDocument: generatePolicy('Allow', '*'),
